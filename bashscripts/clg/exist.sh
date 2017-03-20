d56e2adb68d9bcbcb19019add2c3ee7712ed1ef3
mouri11 <rohit.das950@gmail.com>
2017-03-20 19:40:03 +0530

    Comments

--- bashscripts/clg/exist.sh
+++ bashscripts/clg/exist.sh
@@ -1,5 +1,5 @@
-#A shell script to read a file/directory name from the terminal, check if it is in current directory, and if yes, display if it is a file or directory
 #!/bin/bash
+#A shell script to read a file/directory name from the terminal, check if it is in current directory, and if yes, display if it is a file or directory
 read -p "Enter a file or directory name: " arg
 echo ""
 if [ -f $arg ]
